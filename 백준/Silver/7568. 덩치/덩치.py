b2ba006620da818dca553e65904076505f6ffcb5
Daeyeon Lee <angellee98@naver.com>
2025-11-01 20:21:20 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,13 @@
+N = int(input())
+people = []
+for _ in range(N):
+    x, y = map(int,input().split())
+    people.append([x,y])
+
+cnt = [1] * N
+for i in range(N):
+    for j in range(N):
+        if i != j:
+            if people[i][0] < people[j][0] and people[i][1] < people[j][1]:
+                cnt[i] += 1
+print(*cnt)
